5815ba51f549370cdfc9176c609126633e1ed3ea
ARKAN <a.r.khan.coxyboys@gmail.com>
2023-06-01 06:54:47 +0600

    added header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import './Header.css'
+import logo from'../../images/Logo.svg';
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is a header</h2>
-        </div>
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div>
+                <a href="/shop">Shop</a>
+                <a href="/order">Order</a>
+                <a href="/inventory">Inventory</a>
+                <a href="/loging">Loging</a>
+            </div>
+            
+        </nav>
     );
 };
 
