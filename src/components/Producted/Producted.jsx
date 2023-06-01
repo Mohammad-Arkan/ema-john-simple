026d2c0cc50ad2df5ca14e1292c608419c37345a
Mohammad Arkan <a.r.khan.coxyboys@gmail.com>
2023-06-01 11:11:15 +0600

    added product

--- src/components/Producted/Producted.jsx
+++ src/components/Producted/Producted.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import './Producted.css'
-
+import logo from '../../assets/react.svg'
 const Producted = (props) => {
     const {img, name, seller, price, ratings} = props.product
     return (
         <div className='producted'>
              <img src={img} alt="" />    
-            <div>
+            <div className='producted-info'>
                 <h6>Name:{name}</h6>
-                <p>Price:{seller} </p> 
-                <p>Seller: {price}</p>
-                <p>Rating:{ratings} </p>
+                <p className='price'>Price:{seller} </p> 
+                <p className='seller'>Seller: {price}</p>
+                <p className='rating'>Rating:{ratings} star </p>
             </div>
-            
+            <button className='addToCart'>Add to cart 
+            <img src={logo} alt="" />
+             </button>
         </div>
     );
 };
